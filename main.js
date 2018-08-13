2cf09a4ee6684d5a34445448e19db94f0cfde1ca
Jacoulin <405563383@qq.com>
2018-08-13 23:35:16 +0800

    new

--- main.js
+++ main.js
@@ -7,22 +7,47 @@ const BrowserWindow = electron.BrowserWindow;
 // 保持一个全局的窗口对象，可以不显示，如果没有这个对象，窗口点击关闭的时候，js对象会被gc干掉
 let mainWindow;
 
+let isMaximized = false;
+
+let screenWidth;
+let screenHeight;
+
+let oriBounds;
+let maxBounds;
+
 function createWindow(){
     mainWindow = new BrowserWindow({
         width: 480,
         height: 320,
 
+
+        useContentSize: true,
+        transparent: true,
         frame: false
     });
+
+    screenWidth = electron.screen.getPrimaryDisplay().workAreaSize.width;
+    screenHeight = electron.screen.getPrimaryDisplay().workAreaSize.height;
+    maxBounds = {
+        x: 0,
+        y: 0,
+        width: screenWidth,
+        height: screenHeight
+    };
+
     // 加载静态资源
     mainWindow.loadURL('file://' + __dirname + '/html/index.html');
 
     // 打开开发者工具
     // mainWindow.webContents.openDevTools()
 
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    });
+
     mainWindow.on("closed",function(){
         mainWindow = null
-    })
+    });
 }
 
 app.on("ready",createWindow);
@@ -53,8 +78,7 @@ ipcMain.on('window-min-req', (event, arg) => {
  * 改为使用win.setSimpleFullScreen(true)的方式对窗口进行最大化
  *
  */
-//窗口最大化
-ipcMain.on('window-max-req', (event, arg) => {
+ /*ipcMain.on('window-max-req', (event, arg) => {
     if (mainWindow.isFullScreen()) {
         mainWindow.setSimpleFullScreen(false);
     } else {
@@ -63,7 +87,34 @@ ipcMain.on('window-max-req', (event, arg) => {
     }
     console.log(mainWindow.getSize());
     event.returnValue = mainWindow.isFullScreen();
+});*/
+
+/*ipcMain.on('window-max-req', (event, arg) => {
+
+    if(isMaximized){
+        mainWindow.setBounds(oriBounds);
+        isMaximized = false;
+    }else{
+        oriBounds = mainWindow.getBounds();
+        mainWindow.setBounds(maxBounds);
+        isMaximized = true;
+    }
+
+    event.returnValue = isMaximized;
+
+});*/
+
+ipcMain.on('window-max-req',(event, arg) =>{
+
+    if(mainWindow.isMaximized()){
+        mainWindow.unmaximize();
+    }else{
+        mainWindow.maximize();
+    }
+
+    event.returnValue = mainWindow.isMaximized();
 });
+
 //窗口关闭
 ipcMain.on('window-close-req', (event, arg) => {
     mainWindow.close();
